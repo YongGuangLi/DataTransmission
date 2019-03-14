088d5f293fa0267ed8bc1ae9f38c01368a9fe892
YongGuangLi <liyg0428@icloud.com>
2019-03-14 16:33:37 +0800

    备注另一种传入jdbcTemplate的方式

--- src/main/java/com/example/demo/RedisListenerConfig.java
+++ src/main/java/com/example/demo/RedisListenerConfig.java
@@ -28,8 +28,8 @@ public class RedisListenerConfig {
  
 	@Bean
     MessageListenerAdapter listenerAdapter() {
-		RedisMessageListener redisMessageListener = new RedisMessageListener();
-		redisMessageListener.getStationConnectConfig(jdbcTemplate);
+		RedisMessageListener redisMessageListener = new RedisMessageListener();  
+		redisMessageListener.getStationConnectConfig(jdbcTemplate);               //也可通过RedisMessageListener构造函数传入
         return new MessageListenerAdapter(redisMessageListener);
     }
 	
